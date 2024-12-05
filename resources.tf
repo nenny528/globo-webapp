e84fef7f5d44e2f9d2a914cbd22abd2c525a47a7
Jennifer Moore <Jennifer.Moore@nelnetservicing.net>
2024-12-05 12:55:28 -0600

    Switch to using secrets manager

--- resources.tf
+++ resources.tf
@@ -54,8 +54,8 @@ resource "aws_instance" "main" {
   user_data = templatefile("./templates/userdata.sh", {
     playbook_repository = var.playbook_repository
     secret_id           = var.api_key_secret_id
-    host_list_ssm_name  = var.host_list_ssm_name
-    site_name_ssm_name  = var.site_name_ssm_name
+    host_list_ssm_name  = local.host_list_ssm_name
+    site_name_ssm_name  = local.site_name_ssm_name
   })
 }
 
